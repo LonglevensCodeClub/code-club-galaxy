ac51d650c031aa28bb6fa5cd9c54f0b593b471c9
Fred <fred@codeclub.org>
2017-11-02 23:25:10 +0000

    removing gradients, adding genay project

--- src/animationBoards/galaxy/renderFunctions/planet.js
+++ src/animationBoards/galaxy/renderFunctions/planet.js
@@ -2,16 +2,10 @@ import React from 'react'
 
 export default (element) => (
     <g key={element.id}>
-        <defs>
-            <radialGradient id={`grad${element.id}`} cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-            <stop offset="0%" style={{stopColor: 'white', stopOpacity:1}} />
-            <stop offset="100%" style={{stopColor: element.colour, stopOpacity:1}} />
-            </radialGradient>
-        </defs>
         <circle
             r={element.radius} 
             cx={element.positionX} 
             cy={element.positionY}
-            fill={`url(#grad${element.id})`}/> 
+            fill={element.colour}/> 
     </g>
 )
