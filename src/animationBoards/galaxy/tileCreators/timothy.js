591006a4b8d0e47e7f9b030331a8cca51cf8d941
pi <pi@codeclub.com>
2017-12-02 12:37:11 +0000

    added timothy

---
+++ src/animationBoards/galaxy/tileCreators/timothy.js
@@ -0,0 +1,43 @@
+export default (galaxy) => {
+	// Your code goes inside these curly brackets
+	galaxy.setName('Timothy Beridze!')
+
+	//Light Blue Galaxy	
+//Make A Sun
+//Venus
+//Saturn Is My Favorite Plannet Forever
+//Uranus Is My Least Favorite Plannet
+//I'm Guoaing Krazy
+//Create a sun, assign it to a varible
+
+	const sun = galaxy.newSun({
+		colour: 'orange',
+		radius:40
+	})
+	//Create a sun, assign it to a variable
+	//colour: 'orange', // add this comma
+	const planet = galaxy.newPlanet({
+		colour: 'lightblue' ,
+		radius: 10
+		
+	})
+	planet.orbit(sun, {
+		frequency: -0.02,
+		radiusX: 55,
+		radiusy: 80
+		
+		
+	})
+	// The sun will rotate through these colours
+	const sunColours = [ 'red', 'darked', 'indigo']
+	
+	
+	
+
+
+	
+	
+	
+	
+	
+}
